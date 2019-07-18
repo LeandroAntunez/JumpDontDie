0dfc40e43a46d34585648f3fdd9018ef23b79a53
Leandro Antunez <leandro_antunez@outlook.com>
2019-07-18 19:30:14 -0300

    TextureRegion added for spike.

--- core/src/com/leandro/antunez/game/MainClass.java
+++ core/src/com/leandro/antunez/game/MainClass.java
@@ -5,17 +5,22 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MainClass extends ApplicationAdapter {
 
 	private Texture minijoe;
+	private Texture spike;
+	private TextureRegion spikeRegion;
 	private SpriteBatch batch;
 	private int width, height;
     private float widthJoe, heightJoe;
 	@Override
 	public void create() {
 		minijoe = new Texture("minijoe.png");
+		spike = new Texture("spike.png");
 		batch = new SpriteBatch();
+		spikeRegion = new TextureRegion(spike, 0, 0, spike.getWidth(), spike.getHeight());
 		width = Gdx.graphics.getWidth();
 		height = Gdx.graphics.getHeight();
 		widthJoe = minijoe.getWidth();
@@ -27,6 +32,7 @@ public class MainClass extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		batch.draw(spikeRegion, 200, 0);
 		batch.draw(minijoe, 0, 0);
 		batch.draw(minijoe,0 , height - heightJoe);
 		batch.draw(minijoe, width - widthJoe , 0);
@@ -37,6 +43,7 @@ public class MainClass extends ApplicationAdapter {
 	@Override
 	public void dispose() {
 		minijoe.dispose();
+		spike.dispose();
 		batch.dispose();
 	}
 }
