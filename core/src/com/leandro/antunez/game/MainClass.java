d61f3b581d7d772e6a53292c4055e37fa8af614e
Leandro Antunez <leandro_antunez@outlook.com>
2019-07-18 14:11:33 -0300

    Height, width and position of images.

--- core/src/com/leandro/antunez/game/MainClass.java
+++ core/src/com/leandro/antunez/game/MainClass.java
@@ -10,23 +10,32 @@ public class MainClass extends ApplicationAdapter {
 
 	private Texture minijoe;
 	private SpriteBatch batch;
-
+	private int width, height;
+    private float widthJoe, heightJoe;
 	@Override
 	public void create() {
 		minijoe = new Texture("minijoe.png");
 		batch = new SpriteBatch();
+		width = Gdx.graphics.getWidth();
+		height = Gdx.graphics.getHeight();
+		widthJoe = minijoe.getWidth();
+		heightJoe = minijoe.getHeight();
 	}
 
 	@Override
 	public void render() {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(minijoe, 0 , 0);
+		batch.draw(minijoe, 0, 0);
+		batch.draw(minijoe,0 , height - heightJoe);
+		batch.draw(minijoe, width - widthJoe , 0);
+		batch.draw(minijoe, width - widthJoe , height - heightJoe);
 		batch.end();
 	}
 
 	@Override
 	public void dispose() {
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		minijoe.dispose();
 		batch.dispose();
 	}
