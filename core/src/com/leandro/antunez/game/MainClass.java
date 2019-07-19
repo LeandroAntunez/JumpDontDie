6f078742a58c2ba6902f9af9aa957f2a5a99edaf
Leandro Antunez <leandro_antunez@outlook.com>
2019-07-19 16:11:08 -0300

    Input processor setted.

--- core/src/com/leandro/antunez/game/MainClass.java
+++ core/src/com/leandro/antunez/game/MainClass.java
@@ -2,29 +2,26 @@ package com.leandro.antunez.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MainClass extends ApplicationAdapter {
 
-	@Override
-	public void create() {
+    @Override
+    public void create() {
+        Procesador procesador = new Procesador();
+        Gdx.input.setInputProcessor(procesador);
+    }
 
-	}
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        if (Gdx.input.justTouched()){
+            System.out.println("Estas tocando la pantalla.");
+        }
+    }
 
-	@Override
-	public void render() {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		if (Gdx.input.justTouched()){
-			System.out.println("Estas tocando la pantalla.");
-		}
-	}
-
-	@Override
-	public void dispose() {
-	}
+    @Override
+    public void dispose() {
+    }
 }
