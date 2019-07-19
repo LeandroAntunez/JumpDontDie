741bd883b7e7c45bf6f5718882c28800f52060aa
Leandro Antunez <leandro_antunez@outlook.com>
2019-07-19 16:28:21 -0300

    Multiple Screen added.

---
+++ core/src/com/leandro/antunez/game/MainGame.java
@@ -0,0 +1,19 @@
+package com.leandro.antunez.game;
+
+import com.badlogic.gdx.Game;
+
+public class MainGame extends Game {
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
