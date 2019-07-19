4cdcc55710ef29994735e8e57555d929c95a11c2
Leandro Antunez <leandro_antunez@outlook.com>
2019-07-19 16:59:59 -0300

    Actor added to the screen.

--- core/src/com/leandro/antunez/game/MainGameScreen.java
+++ core/src/com/leandro/antunez/game/MainGameScreen.java
@@ -2,29 +2,31 @@ package com.leandro.antunez.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.leandro.antunez.game.actors.ActorJugador;
 
 public class MainGameScreen extends BaseScreen {
 
     private Stage stage;
-    private ActorJugador actorJugador;
+    private Texture texturaJugador;
 
-    public MainGameScreen(MainGame game) {
+    MainGameScreen(MainGame game) {
         super(game);
+        texturaJugador = new Texture("minijoe.png");
     }
 
     @Override
     public void show() {
         stage = new Stage();
-        actorJugador = new ActorJugador();
+        ActorJugador actorJugador = new ActorJugador(texturaJugador);
         stage.addActor(actorJugador);
         actorJugador.setPosition(20,100);
     }
 
     @Override
     public void hide() {
-        stage.dispose();
+        dispose();
     }
 
     @Override
@@ -33,5 +35,10 @@ public class MainGameScreen extends BaseScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act();
         stage.draw();
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
     }
 }
--- core/src/com/leandro/antunez/game/actors/ActorJugador.java
+++ core/src/com/leandro/antunez/game/actors/ActorJugador.java
@@ -1,17 +1,27 @@
 package com.leandro.antunez.game.actors;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class ActorJugador extends Actor {
+
+    private Texture texturaJugador;
+
+    public ActorJugador(Texture textureJugador){
+        this.texturaJugador = textureJugador;
+    }
 
     @Override
     public void act(float delta) {
         super.act(delta);
     }
 
+
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
     }
+
+
 }
