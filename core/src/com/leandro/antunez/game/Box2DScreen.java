1acfd50c464b4141661f7671de1ccec309225ddc
Leandro Antunez <leandro_antunez@outlook.com>
2019-07-23 17:00:15 -0300

    Renderer and Camera of Box2D added.

--- core/src/com/leandro/antunez/game/Box2DScreen.java
+++ core/src/com/leandro/antunez/game/Box2DScreen.java
@@ -2,12 +2,16 @@ package com.leandro.antunez.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Box2DScreen extends BaseScreen {
 
     private World world;
+    private Box2DDebugRenderer renderer;
+    private OrthographicCamera camera;
 
     public Box2DScreen(MainGame game) {
         super(game);
@@ -16,6 +20,8 @@ public class Box2DScreen extends BaseScreen {
     @Override
     public void show() {
         world = new World(new Vector2(0, -10), true);
+        renderer = new Box2DDebugRenderer();
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     @Override
@@ -23,10 +29,12 @@ public class Box2DScreen extends BaseScreen {
         Gdx.gl.glClearColor(0.4f,0.5f,0.8f,1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         world.step(delta, 6, 2);
+        renderer.render(world, camera.combined);
     }
 
     @Override
     public void dispose() {
         world.dispose();
+        renderer.dispose();
     }
 }
