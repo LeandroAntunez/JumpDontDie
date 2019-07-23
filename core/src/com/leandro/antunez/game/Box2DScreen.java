a274d3bb80d1bb4b51514ee90d87cc94268a20cb
Leandro Antunez <leandro_antunez@outlook.com>
2019-07-23 17:34:14 -0300

    Screen dimensioned.

--- core/src/com/leandro/antunez/game/Box2DScreen.java
+++ core/src/com/leandro/antunez/game/Box2DScreen.java
@@ -27,7 +27,7 @@ public class Box2DScreen extends BaseScreen {
     public void show() {
         world = new World(new Vector2(0, -10), true);
         renderer = new Box2DDebugRenderer();
-        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera = new OrthographicCamera(32, 18);
 
         BodyDef minijoeDef = createJoeBodyDef();
         minijoeBody = world.createBody(minijoeDef);
