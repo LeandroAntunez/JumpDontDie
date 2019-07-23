d850f806d719412bcda0f12788b6e54a76eee0c6
Leandro Antunez <leandro_antunez@outlook.com>
2019-07-23 17:08:25 -0300

    Body and Bodydef of minijoe added.

--- core/src/com/leandro/antunez/game/Box2DScreen.java
+++ core/src/com/leandro/antunez/game/Box2DScreen.java
@@ -4,7 +4,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Box2DScreen extends BaseScreen {
@@ -12,6 +15,8 @@ public class Box2DScreen extends BaseScreen {
     private World world;
     private Box2DDebugRenderer renderer;
     private OrthographicCamera camera;
+    private Fixture minijoeFixture;
+    private Body minijoeBody;
 
     public Box2DScreen(MainGame game) {
         super(game);
@@ -22,6 +27,16 @@ public class Box2DScreen extends BaseScreen {
         world = new World(new Vector2(0, -10), true);
         renderer = new Box2DDebugRenderer();
         camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        BodyDef minijoeDef = createJoeBodyDef();
+        minijoeBody = world.createBody(minijoeDef);
+
+    }
+
+    private BodyDef createJoeBodyDef() {
+        BodyDef def = new BodyDef();
+        def.position.set(0,10);
+        def.type = BodyDef.BodyType.DynamicBody;
+        return def;
     }
 
     @Override
@@ -35,6 +50,7 @@ public class Box2DScreen extends BaseScreen {
     @Override
     public void dispose() {
         world.dispose();
+        world.destroyBody(minijoeBody);
         renderer.dispose();
     }
 }
