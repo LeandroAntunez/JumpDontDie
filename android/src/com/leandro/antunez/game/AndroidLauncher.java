fad9bff27d957b0dfb91d3d9576ac33a1ffd226a
Leandro Antunez <leandro_antunez@outlook.com>
2019-07-26 21:38:43 -0300

    Launcher for Android fixed. Icons added.

--- android/src/com/leandro/antunez/game/AndroidLauncher.java
+++ android/src/com/leandro/antunez/game/AndroidLauncher.java
@@ -1,16 +1,14 @@
 package com.leandro.antunez.game;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.leandro.antunez.game.MainClass;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MainClass(), config);
+		initialize(new MainGame(), config);
 	}
 }
